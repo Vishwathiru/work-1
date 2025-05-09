1dfa0112e9985eb59f6f69820d30bbad279fbb52
Vishwathiru <vishwathiru1104@gmail.com>
2025-05-09 08:05:33 +0530

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,17 @@
+mD2={'name':'Magesh',1:[1,2,3,4,5]}
+mD3=mD2.fromkeys((10,20,30))
+print(mD3)
+mD1={1:'Apple',2:'Banana',3:'Guava',4:'Mango',5:'Orange'}
+k=mD1.keys()
+print(k)
+v=mD1.values()
+print(v)
+i=mD1.items()
+print(i)
+l=len(mD1)
+print(l)
+c=mD1.copy()
+s=sorted(mD1)
+print(s)
+cmp=cmp(mD1,mD2)
+print(cmp)
